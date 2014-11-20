99841d14b71b46159669d9a7bfe0817c1591cae3
Verdi R-D <verdi@azend.org>
2014-11-19 20:09:13 -0500

    Improved routing and added default + error routes

--- routes.php
+++ routes.php
@@ -2,6 +2,8 @@
 
 $routes = array(
 	'/hello' => array( 'controller' => 'HelloWorldController', 'action' => 'index', 'method' => 'get')
+	'default' => array( 'controller' => 'HelloWorldController', 'action' => 'index', 'method' => 'get')
+	'error' => array( 'controller' => 'HelloWorldController', 'action' => 'index', 'method' => 'get')
 
 );
 
@@ -9,17 +11,30 @@ $controller = 'HelloWorldController';
 $action = 'index';
 $method = 'get';
 
+$selectedRoute = 
+
 if (isset($_GET['path']) && !empty($_GET['path'])) {
 	$path = $_GET['path'];
 
 	foreach($routes as $route => $opts) {
 		if ($route === $path) {
-			$controller = $opts['controller'];
-			$action = $opts['action'];
-
-			$instance = new $controller();
-			$instance->$action();
+			$selectedRoute = $route;
 		}	
+		break;
+	}
+
+	if (!$selectedRoute) {
+		$selectedRoute = $routes['error'];
 	}
 
 }
+else {
+	$selectedRoute = $route['default'];
+}
+
+$controller = $selectedRoute['controller'];
+$action = $selectedRoute['action'];
+$method = $selectedRoute['method'];
+
+$instance = new $controller();
+$instance->$action();
