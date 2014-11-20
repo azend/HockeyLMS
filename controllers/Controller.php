0f66a66df5b676843fefa2f8ba428cafebd9c202
Verdi R-D <verdi@azend.org>
2014-11-19 20:42:44 -0500

    Added render of views ability to Controller

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -1,5 +1,30 @@
 <?php
 
 class Controller {
+	protected $viewBag = array();
 
+	function render() {
+		$functionName = getCallingFunction();
+		$viewFileName = buildViewFileName($functionName);
+		
+		if (file_exists($viewFileName)) {
+			isolateRender($viewFileName);
+		}
+	}
+
+	private isolateRender($viewPath) {
+		explode($viewBag);
+		include($viewPath);
+	}
+
+	private function buildViewFileName($functionName) {
+		return '../views/' . $functionName . '.php';
+	}
+
+	private function getCallingFunction() {
+		$stack = debug_backtrace();
+		$callingFunction = $stack[2]['function'];
+
+		return $stack;
+	}	
 }
