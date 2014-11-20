67f89b39c3cd214466a04158393d5850a015e968
Verdi R-D <verdi@azend.org>
2014-11-20 00:27:47 -0500

    Added postback to login form

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -4,4 +4,16 @@ class LoginController extends Controller {
 	function index () {
 		$this->render();
 	}
+
+	function login () {
+		$fv = new FormValidator();
+
+		$fv->enforceMethod('POST');
+		$fv->enforce('email', array('required' => true));
+		$fv->enforce('password', array('required' => true));
+		
+		if($fv->validate()) {
+			print_r($_POST);
+		}
+	}
 }
--- routes.php
+++ routes.php
@@ -2,7 +2,7 @@
 
 $routes = array(
 	'/hello' => array( 'controller' => 'HelloWorldController', 'action' => 'index', 'method' => 'get'),
-	'/login' => array( 'controller' => 'LoginController', 'action' => 'index', 'method' => 'get'),
+	'/login' => array( 'controller' => 'LoginController', 'action' => 'login', 'method' => 'post'),
 	'default' => array( 'controller' => 'HelloWorldController', 'action' => 'index', 'method' => 'get'),
 	'error' => array( 'controller' => 'HelloWorldController', 'action' => 'index', 'method' => 'get')
 
