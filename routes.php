d507854765d6a12eb1a671049823d9a03ea5462e
Verdi R-D <verdi@azend.org>
2014-11-19 20:17:20 -0500

    Fixed incorrect symbol

--- routes.php
+++ routes.php
@@ -25,7 +25,7 @@ if (isset($_GET['path']) && !empty($_GET['path'])) {
 
 }
 else {
-	$selectedRoute = $route['default'];
+	$selectedRoute = $routes['default'];
 }
 
 $controller = $selectedRoute['controller'];
