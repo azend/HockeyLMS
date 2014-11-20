1d2742ae25f8e4fb1b750029746319411723e48d
Verdi R-D <verdi@azend.org>
2014-11-20 15:59:55 -0500

    Fixed property reference

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -14,7 +14,7 @@ class DashboardController extends Controller {
 	}
 
 	private function checkLogin() {
-		if (!$lh->isLoggedIn()) {
+		if (!$this->lh->isLoggedIn()) {
 			header('Location: ?path=/login');
 			die();
 		}
