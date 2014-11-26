808e55ae0d05905e439be49da4ab47100cdd327d
Verdi R-D <verdi@azend.org>
2014-11-26 15:02:38 -0500

    Added form to start password reset process

--- views/Login/index.php
+++ views/Login/index.php
@@ -91,7 +91,13 @@
         </div>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
       </form>
-
+      <hr>
+      <form class="form-signin" role="form" action="?path=/forgotPassword" method="post">
+        <h2 class="form-signin-heading">Forgotten password?</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email address" required autofocus>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Reset</button>
+      </form>
     </div> <!-- /container -->
 
 
